0850db0a3061ec94645df06faede915c88cc93bd
Aditya Dutta <adidutta.funny@gmail.com>
2021-12-21 23:41:08 +0530

    First personal build

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,19 @@
+#define PATH(x) "$HOME/src/dwmblocks/modules/"x
+
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/* {"",              PATH("mpd"),                                                  0,      4}, */
+	{"",              PATH("internet"),                                              0,      3},
+	/* {"",              PATH("calcurse_block"),                                       5,      0}, */
+	/* {"",              PATH("bluetooth"),                                            5,      0}, */
+	{"",              PATH("battery"),                                              5,      0},
+	{"",              PATH("memory"),                                               10,     0},
+	{"",              PATH("vol"),                                                  0,      1},
+	{"",              PATH("brightness"),                                           0,      2},
+	{"",              PATH("datetime"),					                            5,	    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
